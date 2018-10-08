d657178e85142fc7e86c9d4dd29f9120953bc37b
qinshuai <audie_nt@163.com>
2018-10-08 09:51:58 +0800

    init

---
+++ src/main/java/com/magic/house/Application.java
@@ -0,0 +1,19 @@
+package com.magic.house;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ *
+ * @author lenovo
+ * @date 2018/10/8
+ * 描述：启动类
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

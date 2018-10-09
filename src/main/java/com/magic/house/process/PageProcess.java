ad1a9677d7baebc64bf5ca6756c652b54ccd3f90
qinshuai <audie_nt@163.com>
2018-10-09 17:19:55 +0800

    init

--- src/main/java/com/magic/house/process/PageProcess.java
+++ src/main/java/com/magic/house/process/PageProcess.java
@@ -1,11 +1,17 @@
 package com.magic.house.process;
 
+import java.io.*;
 import java.util.List;
 
+import jdk.internal.util.xml.impl.Input;
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.HttpRequestBody;
 import us.codecraft.webmagic.processor.PageProcessor;
+
+import javax.swing.text.html.HTML;
 
 
 public class PageProcess implements PageProcessor {
@@ -15,12 +21,19 @@ public class PageProcess implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        List<String> imgs = page.getHtml().css("div.key-list").css("div.item-mod").css("img","src").all();
-        List<String> names= page.getHtml().css("div.key-list").xpath("//span[@class='items-name']").css("span","text").all();
-        List<String> houseType= page.getHtml().css("div.key-list").css("a.huxing").all();
-        List<String> prices= page.getHtml().css("div.key-list").xpath("//a[@class='favor-pos']").xpath("//p[@class='price]").all();
 
+//        System.out.println(page.getHtml());
 
+//        List<String> imgs = page.getHtml().css("div.key-list").css("div.item-mod").css("img","src").all();
+//        List<String> names= page.getHtml().css("div.key-list").xpath("//span[@class='items-name']").css("span","text").all();
+//        List<String> houseType= page.getHtml().css("div.key-list").css("a.huxing").all();
+//        List<String> prices= page.getHtml().css("div.key-list").xpath("//a[@class='favor-pos']").xpath("//p[@class='price]").all();
+
+        List<String> datalink=page.getHtml().css("div.key-list").css("div.item-mod","data-link").all();
+        page.addTargetRequests(datalink);
+
+        buildHtml(page.getBytes());
+        System.out.println(page.getHtml());
     }
 
     @Override
@@ -28,8 +41,30 @@ public class PageProcess implements PageProcessor {
         return site;
     }
 
+    public static void buildHtml(byte[] bytes){
+        Long time=System.currentTimeMillis();
+        File file=new File("d:/h/"+time+".html");
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileOutputStream fileOutputStream=new FileOutputStream(file);
+            fileOutputStream.write(bytes);
+            fileOutputStream.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         Spider.create(new PageProcess()).addUrl("https://zz.fang.anjuke.com/?from=navigation").thread(5).run();
-
     }
+
 }
